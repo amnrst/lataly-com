29c5961b95f9646f7e28d69fdcd1caa0dbef880a
amin <amin.roosta@outlook.com>
2022-11-12 17:22:48 -0500

    Move more css to tailwind

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,7 +5,7 @@ import { RouterLink } from "vue-router";
 <template>
   <div class="greetings">
     <h1 class="pb-8 text-3xl text-center text-teal-600">Latlay!</h1>
-    <nav class="text-center">
+    <nav class="text-center text-xs w-full">
       <RouterLink to="/" class="border-r pr-4">Home</RouterLink>
       <RouterLink to="/about" class="pl-4">About</RouterLink>
     </nav>
@@ -13,16 +13,7 @@ import { RouterLink } from "vue-router";
 </template>
 
 <style scoped>
-nav {
-  width: 100%;
-  font-size: 12px;
-}
-
 nav a.router-link-exact-active {
   color: #14b8a6;
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
 }
 </style>
