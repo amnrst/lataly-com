cc3f4000f1818aff477a816ed22d70b1f959570d
amin <amin.roosta@outlook.com>
2022-11-12 17:12:58 -0500

    Add tailwindcss

---
+++ src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="pb-8 text-3xl text-center text-teal-600">Latlay!</h1>
+    <nav class="text-center">
+      <RouterLink to="/" class="border-r pr-4">Home</RouterLink>
+      <RouterLink to="/about" class="pl-4">About</RouterLink>
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+nav {
+  width: 100%;
+  font-size: 12px;
+}
+
+nav a.router-link-exact-active {
+  color: #14b8a6;
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+</style>
